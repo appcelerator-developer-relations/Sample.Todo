b559275a79a56f32de3274251b7663055aa6b225
tonylukasavage <anthony.lukasavage@gmail.com>
2011-11-28 21:57:10 -0500

    using ROWID and preseeded DB now

--- Resources/db.js
+++ Resources/db.js
@@ -1,23 +1,15 @@
-var dbid;
+var DATABASE_NAME = 'todo';
 
 exports.createDb = function() {
-	var mydb = Ti.Database.open('mydb');
-	mydb.execute('create table if not exists todo (id integer, item text, done integer)');
-	mydb.execute('delete from todo');
-	mydb.execute('insert into todo (id,item,done) values (?,?,?)',0,'Pick Up Laundry',0);
-	mydb.execute('insert into todo (id,item,done) values (?,?,?)',1,'Go Food Shopping',0);
-	mydb.execute('insert into todo (id,item,done) values (?,?,?)',2,'Call Mom',0);
-	mydb.execute('insert into todo (id,item,done) values (?,?,?)',3,'Sleep',1);
-	dbid = mydb.lastInsertRowId;
-	mydb.close();
+	Ti.Database.install('todo.sqlite', DATABASE_NAME);
 };
 
 exports.selectItems = function(_done) {
 	var retData = [];
-	var db = Ti.Database.open('mydb');
-	var rows = db.execute('select * from todo where done = ?', _done);
+	var db = Ti.Database.open(DATABASE_NAME);
+	var rows = db.execute('select ROWID, * from todo where done = ?', _done);
 	while (rows.isValidRow()) {
-		retData.push({item:rows.fieldByName('item'), id:rows.fieldByName('id')});
+		retData.push({item:rows.fieldByName('item'), id:rows.fieldByName('ROWID')});
 		rows.next();
 	}
 	db.close();
@@ -25,15 +17,15 @@ exports.selectItems = function(_done) {
 };
 
 exports.updateItem = function(_id, _done) { 
-	var mydb = Ti.Database.open('mydb');
-	mydb.execute('update todo set done = ? where id = ?', _done, _id);
+	var mydb = Ti.Database.open(DATABASE_NAME);
+	mydb.execute('update todo set done = ? where ROWID = ?', _done, _id);
 	var rows = mydb.execute('select * from todo where done = ?', _done);
 	mydb.close();
 	return rows;
 };
 
 exports.addItem = function(_item) {
-	var mydb = Ti.Database.open('mydb');
-	mydb.execute('insert into todo values (?,?,?)', dbid++, _item, 0);
+	var mydb = Ti.Database.open(DATABASE_NAME);
+	mydb.execute('insert into todo values (?,?)', _item, 0);
 	mydb.close();
 };

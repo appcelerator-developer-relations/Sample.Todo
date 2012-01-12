6e25ba5b86188013a0443deb8000ce20d6871a7d
Tony Lukasavage <anthony.lukasavage@gmail.com>
2012-01-12 15:12:07 -0500

    enforcing 1.8+ and not mobile web (for now)

--- Resources/app.js
+++ Resources/app.js
@@ -1,49 +1,57 @@
-//add a single variable to the global scope to which we may choose to
-//intentionally add items to
-var globals = {};
-
-//create a private scope to prevent further polluting the global object
-(function() {
-	var AppTabGroup = require('ui/AppTabGroup').AppTabGroup,
-		ListWindow = require('ui/ListWindow').ListWindow,
-		AddWindow = require('ui/AddWindow').AddWindow;
+if (Ti.version < 1.8 ) {
+	alert('Sorry - this application template requires Titanium Mobile SDK 1.8 or later');
+}
+else if (Ti.Platform.osname === 'mobileweb') {
+	alert('Mobile web is not yet supported by this template');
+}
+else {
+	//add a single variable to the global scope to which we may choose to
+	//intentionally add items to
+	var globals = {};
 	
-	// Initialize local storage
-	require('db').createDb();
-	
-	//create our global tab group	
-	globals.tabs = new AppTabGroup(
-		{
-			title: 'Todo',
-			icon: 'images/KS_nav_ui.png',
-			window: new ListWindow({
+	//create a private scope to prevent further polluting the global object
+	(function() {
+		var AppTabGroup = require('ui/AppTabGroup').AppTabGroup,
+			ListWindow = require('ui/ListWindow').ListWindow,
+			AddWindow = require('ui/AddWindow').AddWindow;
+		
+		// Initialize local storage
+		require('db').createDb();
+		
+		//create our global tab group	
+		globals.tabs = new AppTabGroup(
+			{
 				title: 'Todo',
-				backgroundColor: '#fff',
-				navBarHidden: false,
-				isDone: 0,
-				activity: {
-					onCreateOptionsMenu: function(e) {
-						var menu = e.menu;
-					    var menuItem = menu.add({ title: "Add Task" });
-					    menuItem.setIcon("images/ic_menu_add.png");
-					    menuItem.addEventListener("click", function(e) {
-					        new AddWindow().open();
-					    });
+				icon: 'images/KS_nav_ui.png',
+				window: new ListWindow({
+					title: 'Todo',
+					backgroundColor: '#fff',
+					navBarHidden: false,
+					isDone: 0,
+					activity: {
+						onCreateOptionsMenu: function(e) {
+							var menu = e.menu;
+						    var menuItem = menu.add({ title: "Add Task" });
+						    menuItem.setIcon("images/ic_menu_add.png");
+						    menuItem.addEventListener("click", function(e) {
+						        new AddWindow().open();
+						    });
+						}
 					}
-				}
-			})
-		},
-		{
-			title: 'Done',
-			icon: 'images/KS_nav_views.png',
-			window: new ListWindow({
+				})
+			},
+			{
 				title: 'Done',
-				backgroundColor: '#fff',
-				navBarHidden: false,
-				isDone: 1
-			})
-		}
-	);
-
-	globals.tabs.open();
-})();
+				icon: 'images/KS_nav_views.png',
+				window: new ListWindow({
+					title: 'Done',
+					backgroundColor: '#fff',
+					navBarHidden: false,
+					isDone: 1
+				})
+			}
+		);
+	
+		globals.tabs.open();
+	})();
+}

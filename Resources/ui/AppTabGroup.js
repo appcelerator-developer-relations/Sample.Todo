ceeb6b85ac5e65388a17ade0fae0102c1ca2b88e
Vui Nguyen <vui_engineer@yahoo.com>
2015-06-28 22:59:12 -0600

    Place "Add" menu item code to AppTabGroup.js
    Place the "Add" menu option code for Android in this file. It should be attached to the Todo tab.

--- Resources/ui/AppTabGroup.js
+++ Resources/ui/AppTabGroup.js
@@ -1,4 +1,6 @@
 exports.AppTabGroup = function() {
+	
+	var AddWindow = require('ui/AddWindow').AddWindow;
 	var self = Ti.UI.createTabGroup();
 
 	//loop through tab objects and add them to the tab group
@@ -14,7 +16,33 @@ exports.AppTabGroup = function() {
 	//track the current tab for the tab group
 	self.addEventListener('focus', function(e) {
 		self.currentTab = e.tab;
+		// clear the action bar menu when switching between tabs for Android
+		if (Ti.Platform.name === "android") {
+			self.getActivity().invalidateOptionsMenu(); 
+		} 
 	});
+
+	if (Ti.Platform.name === "android") {
+    	self.addEventListener("open", function(e) {
+        	var activity = self.getActivity();
+        	activity.onCreateOptionsMenu = function(e) {
+            	var item, menu;
+            	menu = e.menu;
+            	menu.clear();
+    			// if it is the Todo tab on Android, add menu item to action bar
+            	if (!self.activeTab.window.isDone) {
+                	item = menu.add({
+                    	title : "Add",
+                    	icon : "images/images/ic_menu_add.png",
+                    	showAsAction: Ti.Android.SHOW_AS_ACTION_IF_ROOM
+                	});
+                	item.addEventListener("click", function(e) {
+								new AddWindow().open();
+					});
+            	} // end check for isDone variable 
+    		};	// end createOptionsMenu
+    	});
+    }
 
 	return self;
 };
